358f9fb5243de2e0016683ec93d4f2ace5fb3385
Wande Adesulu <wande.adesulu@elevavi.com>
2023-10-12 19:14:18 +0100

    commit 3

--- src/app/components/Grid2.tsx
+++ src/app/components/Grid2.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='text-2xl py-6 bg-white text-blue-900 px-4 flex items-center justify-center
-                    text-center'>United States Trademark: MFL1</div>
+                    text-center'>USA Trademark: MFL1</div>
   )
 }
 export default App
