024f918407536588ccb58e717c51a9349d3fc266
Wande Adesulu <wande.adesulu@elevavi.com>
2023-10-12 19:12:03 +0100

    commit 3

--- src/app/components/Grid3.tsx
+++ src/app/components/Grid3.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-2xl h-12 bg-white text-blue-900 px-4 flex items-center justify-center
+    <div className='text-2xl py-6 bg-white text-blue-900 px-4 flex items-center justify-center
                     text-center'>United Kingdom Trademark: MFL1</div>
   )
 }
--- src/app/components/Grid8.tsx
+++ src/app/components/Grid8.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-2xl mt-28 bg-white text-blue-900 px-4 flex items-center justify-center
+    <div className='text-2xl py-28 bg-white text-blue-900 px-4 flex items-center justify-center
                     text-center'>Contact wande.adesulu@MFL1.com or wandesulu@gmail.com</div>
   )
 }
